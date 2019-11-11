37c16d3cc69271d17403f7809833a776fcddcd29
fujiwara749 <fura3n5@gmail.com>
2019-11-11 10:49:53 +0000

    管理者TOP作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  namespace :admin do
+  	get 'top', to: 'top#top'
+  	resources :users, only: [:index, :show, :edit, :update, :destroy] do
+    end
+  end
   devise_for :users
   get 'home/index'
   get 'home/show'
