aad01abfddd6f9fffab7731d8358f295fd287d09
fujiwara749 <fura3n5@gmail.com>
2019-11-26 07:23:42 +0000

    修正

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,7 +2,7 @@ class Board < ApplicationRecord
 	
 	validates :thread_title, presence: true
 
-	has_many :comments, dependent: :destroy
+	has_many :comments
 	belongs_to :user
 	belongs_to :category  
 end
